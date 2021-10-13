9b4c02ab06a7da2268fdb94124ea88c9a5aad007
Mostafa Saleh <mostafasaleh2055@gmail.com>
2021-10-13 13:52:45 +0200

    acquiring credentials and creating global variable

--- website/app.js
+++ website/app.js
@@ -1,4 +1,9 @@
 /* Global Variables */
+/**
+ * Declaring the OpenWeatherMap's credentials.
+ */
+const apiKey = "9a58e300e2613900f60cd02f3a331e03"; // this is the default secure key of the api for my account.
+const baseUrl = "http://localhost:1999/"; // this is the base URL for the local server.
 
 // Create a new date instance dynamically with JS
 let d = new Date();
