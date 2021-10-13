d80df9296398575254b3c5d26bb23637ecc43854
Mostafa Saleh <mostafasaleh2055@gmail.com>
2021-10-13 12:02:56 +0200

    setting up express

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+/**
+ * Require Express to run server and routes
+ */
+const express = require("express"); // requiring express in our project's server-side code.
 
-// Start up an instance of app
+/**
+ * Start up an instance of app
+ */
+const app = express(); // starting up the express framework instance inside our project.
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); // making body-parser use simple algorithm for parsing.
+app.use(bodyParser.json()); // making the body-parser use the JavaScript Object Notation.
 
 // Cors for cross origin allowance
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('website')); // making access to the client-side code.
 
 
 // Setup Server
