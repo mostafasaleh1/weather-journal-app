0c98d9cece3cedf5340148ecb84a18d8df798a17
Mostafa Saleh <mostafasaleh2055@gmail.com>
2021-10-13 16:33:37 +0200

    fixing bug, server is not working

--- server.js
+++ server.js
@@ -43,6 +43,27 @@ app.use(express.static('website')); // making access to the client-side code.
  * Seting up Server.
  */
 const port = 1999; // creating a port
-const server = listen(port, () => { // creating a variable called server and assigning this fuction to test if the server is running through node.js or not.
+app.listen(port, () => { // creating a variable called server and assigning this fuction to test if the server is running through node.js or not.
     console.log(`the server is running on localhost:${port}`); // printing that the server is running on local host of number = port.
+});
+
+
+/**
+ * Creating a GET route.
+ */
+app.get("/all", (req, res) => { // This function indicates that whenever the link http://localhost:1999/all is visited the server would respond with the projectData object.
+    res.send(projectData); 
+});
+
+
+/**
+ * Creating a POST route.
+ */
+app.post("/post", (req, res) => { // // This function indicates that whenever the link http://localhost:1999/post is visited the server would respond with the projectData object.
+    projectData = { // here we've changed the content of the projectData object to match the ruberic.
+        temperature: req.body.temperature, // I've changed the content of the temperature property to match the body of the request.
+        date: req.body.date, // I've changed the content of the date property to match the body of the request.
+        userResponse: req.body.userResponse, // I've changed the content of the userResponse property to match the body of the request.
+    };
+    res.send(projectData);
 });
