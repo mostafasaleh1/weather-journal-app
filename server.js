b1c30bb41e8da6d0911d7c7c4c4ff730e9c235a4
Mostafa Saleh <mostafasaleh2055@gmail.com>
2021-10-13 12:11:36 +0200

    starting up our dependencies and making the app use them

--- server.js
+++ server.js
@@ -12,11 +12,15 @@ const express = require("express"); // requiring express in our project's server
 const app = express(); // starting up the express framework instance inside our project.
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* 1- Here we are configuring express to use body-parser as middle-ware.*/
+const bodyParser = require("body-parser"); // requiring body-parser in our project's server-side code.
+/* 2- instructing out application to use the body parser package and do the next illustrated steps.*/
 app.use(bodyParser.urlencoded({ extended: false })); // making body-parser use simple algorithm for parsing.
 app.use(bodyParser.json()); // making the body-parser use the JavaScript Object Notation.
 
 // Cors for cross origin allowance
+const cors = require("cors"); // requiring CORS in our project's server-side code.
+app.use(cors()); // Instructing our app to use the CORS package.
 
 // Initialize the main project folder
 app.use(express.static('website')); // making access to the client-side code.
